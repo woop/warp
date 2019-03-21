624390bce3fab94610a80f045c3487799736e989
Willem Pienaar <git@willem.co>
2019-03-21 00:33:38 -0400

    chore: Add support for log file testing

--- test_warp.py
+++ test_warp.py
@@ -1,5 +1,24 @@
 from warp import Warp
+import os
+import time
+
+PATH_TO_OUTPUT_FILE="dummy_server.log"
+
 
 def test_start_process_sdk():
     wp = Warp(command="dummy_server.py")
     wp.start()
+
+    if not os.path.exists(PATH_TO_OUTPUT_FILE):
+        raise Exception("Dummy server isn't producing any output")
+
+    with open(PATH_TO_OUTPUT_FILE) as f:
+        initial_lines = f.readlines()
+
+    # We expect about one log line per second
+    time.sleep(5)
+
+    with open(PATH_TO_OUTPUT_FILE) as f:
+        final_lines = f.readlines()
+
+    assert len(initial_lines) + 5 == len(final_lines)
