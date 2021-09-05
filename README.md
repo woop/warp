c2e3bb2904e3f7472a6422c5c1ebfc33cb137a1b
Willem Pienaar <git@willem.co>
2021-09-05 01:27:39 -0400

    chore: Add readme and setup.py

---
+++ README.md
@@ -0,0 +1,14 @@
+# WARP: Wrapper around Replicated Processes
+
+Installation
+`pip install warp`
+
+Usage
+```
+from warp import Warp
+from nucleonic import nucleonic_config
+# set up your config
+warp = Warp(nucleonic_config)
+```
+
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='warp',
+    description='WARP: Wrapper around Replicated Processes',
+    author='Willem Pienaar',
+    license="Apache-2.0",
+    author_email='pypi@willem.co',
+    url='https://github.com/woop/warp',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
